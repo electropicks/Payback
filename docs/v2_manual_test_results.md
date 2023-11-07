cde564091574293fc6ea295a940c3b57e9021539
jangomax <jangomaxstudio@gmail.com>
2023-11-07 02:50:16 -0800

    V2

---
+++ docs/v2_manual_test_results.md
@@ -0,0 +1,81 @@
+### Workflow 2: Tracking Expenses
+
+**User Story**: Vasanth just got back from a grocery trip with his roommates, where one of his roommates, Suhanth just paid for all of the purchases. He wants to make sure everyone is aware of how much they have to pay Suhanth since they don't all eat the same foods and don't want to pay for groceries they won't be able to make use of. He already has a group set up with all of his roommates so he just needs to start adding the expenses.
+- He makes multiple POST requests to `/api/groups/{group_id}/trips/{expense_id}/add` to add all of the items individually. He includes that Suhanth made the purchase for each item, and passes in the names of all roommates that will be paying for each item. 
+- He then makes a GET request to `/api/groups/{group_id}/calculate` to see how much each person owes Suhanth.
+- After finding out how much he owes, he Venmos Suhanth and makes a POST request to `/api/groups/{group_id}/transactions` to record how much he paid him.
+
+curl -X 'POST' \
+  'http://127.0.0.1:3000/groups/1/trips/1/add' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "userId": "1",
+  "items": [
+    {
+      "name": "Banana",
+      "price": 5,
+      "quantity": 1,
+      "optedOut": []
+    }
+  ]
+}'
+
+"OK"
+
+curl -X 'POST' \
+  'http://127.0.0.1:3000/groups/calculate' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "userId": "1"
+}'
+
+[
+  {
+    "userId": 2,
+    "amount": 33
+  },
+  {
+    "userId": 1,
+    "amount": 20
+  }
+]
+
+### Workflow 3: Correcting an Error
+**User Story**: Mark argues with his roommate over the fact that he paid him for lunch yesterday, but his roommate is convinced that he didn't. 
+- Mark decides to verify the transactions through the app and makes a GET request to `/api/groups/{group_id}/transactions` to show his roommate.
+- His roommate then shows that Mark never actually sent him the payment despite recording it in the app, and mark realizes he forgot to actually pay his roommate. It turns out he also doesn't have enough money to pay him back at the moment, so he decides to remove that transaction for the time being to accurately represent how much he owes his roommate in the app, so he makes a DELETE request to `/api/groups/{group_id}/transactions`
+
+curl -X 'GET' \
+  'http://127.0.0.1:3000/groups/1/transactions' \
+  -H 'accept: application/json'
+
+{
+  "transactions": [
+    {
+      "transaction_id": 1,
+      "from_user_id": 1,
+      "to_user_id": 2,
+      "description": "Paid for trip",
+      "date": "2023-11-07T10:05:52.826359+00:00"
+    },
+    {
+      "transaction_id": 2,
+      "from_user_id": 1,
+      "to_user_id": 1,
+      "description": "Paid for trip",
+      "date": "2023-11-07T10:05:52.826359+00:00"
+    }
+  ]
+}
+
+curl -X 'POST' \
+  'http://127.0.0.1:3000/groups/1/trips/1/delete' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "transaction_id": "5"
+}'
+
+"OK"
--- src/api/groups.py
+++ src/api/groups.py
@@ -2,9 +2,10 @@ import sqlalchemy
 from fastapi import APIRouter, Depends
 from pydantic import BaseModel
 from sqlalchemy.orm import Session
+from typing import List
 
 from src import database as db
-from src.database import get_db, Transaction, GroupMember
+from src.database import get_db, Transaction, ShoppingTrip, GroupMember, Group, User
 
 router = APIRouter(
     prefix="/groups",
@@ -17,22 +18,21 @@ class User(BaseModel):
 
 
 @router.post("/")
-def get_user_groups(user: User, db: Session = Depends(get_db)):
+def get_user_groups(user: User):
     """
     Gets all groups that the user is in.
     """
-    # Assuming 'User' model has a relationship 'group_memberships' to 'GroupMember' model
-    # and 'GroupMember' model has a 'group' relationship to 'Group' model
-    user_groups = (
-        db.query(Group.name)
-        .join(GroupMember, GroupMember.group_id == Group.id)
-        .join(User, User.id == GroupMember.user_id)
-        .filter(User.id == int(user.userId))
-        .all()
-    )
+    with db.engine.begin() as connection:
+        groups = connection.execute(sqlalchemy.text(
+          """
+          SELECT groups.id, groups.name FROM groups
+          JOIN group_members ON group_members.group_id = groups.id
+          JOIN users ON users.id = group_members.user_id AND users.id = :userId
+          """
+        ), {"userId": int(user.userId)})
 
     # Convert query results to list of dictionaries as required by the API response format
-    json = [{"name": group.name} for group in user_groups]
+    json = [{"name": group.name, "groupId": group.id} for group in groups]
     return json
 
 
@@ -64,18 +64,172 @@ def join_group(group_id: int, user_id: int, db: Session = Depends(get_db)):
 
     return {"message": "User joined the group successfully."}
 
-
 @router.get("/{group_id}/transactions")
-def list_group_transactions(group_id: int, user_id: int, db: Session = Depends(get_db)):
-    transactions = db.query(Transaction).filter(Transaction.group_id == group_id).all()
-    payload = []
-    for transaction in transactions:
-        payload.append({
-            "transaction_id": transaction.id,
-            "from_user_id": transaction.from_user_id,
-            "to_user_id": transaction.to_user_id,
-            "amount": transaction.amount,
-            "description": transaction.description,
-            "date": transaction.date
-        })
-    return {"transactions": payload}
+def list_group_transactions(group_id: int):
+    with db.engine.begin() as connection:
+        transactions = connection.execute(sqlalchemy.text(
+            """
+            SELECT *
+            FROM transactions
+            JOIN transaction_ledger ON transactions.id = transaction_ledger.transaction_id
+            WHERE transactions.group_id = :group_id
+            """
+        ), {"group_id": group_id})
+
+
+        payload = []
+        for transaction in transactions:
+            payload.append({
+                "transaction_id": transaction.id,
+                "from_user_id": transaction.from_id,
+                "to_user_id": transaction.to_id,
+                "description": transaction.description,
+                "date": transaction.timestamp
+            })
+
+        return {"transactions": payload}
+
+
+@router.get("/{group_id}/trips")
+def list_group_trips(group_id: int):
+    with db.engine.begin() as connection:
+        trips = connection.execute(sqlalchemy.text(
+            """
+            SELECT id, description, created_at
+            FROM shopping_trips
+            WHERE shopping_trips.group_id = :group_id
+            """
+        ), {"group_id": group_id})
+        payload = []
+        for trip in trips:
+            amount = connection.execute(sqlalchemy.text(
+                """
+                SELECT ROUND(SUM(quantity * price)::numeric, 2) FROM line_items
+                WHERE line_items.trip_id = :trip_id
+                """
+            ), {"trip_id": trip.id}).scalar_one()
+            payload.append({
+                "trip_id": trip.id,
+                "amount": amount,
+                "description": trip.description,
+                "created_at": trip.created_at
+            })
+        return {"trips": payload}
+
+
+
+class Transaction(BaseModel):
+    transaction_id: str
+@router.post("/{group_id}/trips/{trip_id}/delete")
+def delete_transaction(transaction_id: Transaction, group_id: int, trip_id: int):
+    with db.engine.begin() as connection:
+        connection.execute(sqlalchemy.text(
+            """
+            DELETE FROM transactions
+            WHERE id = :id
+            """
+        ), {"id": transaction_id})
+        return "OK"
+
+class Trip(BaseModel):
+    userId: str
+    description: str
+
+@router.post("/{group_id}/trips")
+def create_trip(group_id: int, trip: Trip):
+    with db.engine.begin() as connection:
+        trip_id = connection.execute(sqlalchemy.text(
+            """
+            INSERT INTO shopping_trips (description, payer_id, group_id)
+            VALUES (:desc, :payer, :group)
+            RETURNING id
+            """
+        ), {"desc": trip.description, "payer": trip.userId, "group": group_id}).scalar_one()
+        return {"tripId": trip_id}
+
+@router.get("/{group_id}/trips/{trip_id}")
+def get_trip(group_id: int, trip_id: int):
+    with db.engine.begin() as connection:
+        trip = connection.execute(sqlalchemy.text(
+            """
+            SELECT description, created_at
+            FROM shopping_trips
+            WHERE shopping_trips.id = :trip_id
+            """
+        ), {"group_id": group_id, "trip_id": trip_id}).first()
+        amount = connection.execute(sqlalchemy.text(
+            """
+            SELECT ROUND(SUM(quantity * price)::numeric, 2) FROM line_items
+            WHERE line_items.trip_id = :trip_id
+            """
+        ), {"trip_id": trip_id}).scalar_one()
+        return {
+            "description": trip.description,
+            "amount": amount,
+            "createdAt": trip.created_at
+        }
+
+class Item(BaseModel):
+    name: str
+    price: float
+    quantity: int
+    optedOut: List[int]
+class AddLineItem(BaseModel):
+    userId: str
+    items: List[Item]
+
+@router.post("/{group_id}/trips/{trip_id}/add")
+def add_line_item(body: AddLineItem, group_id: int, trip_id: int):
+    with db.engine.begin() as connection:
+        for item in body.items:
+            line_item_id = connection.execute(sqlalchemy.text(
+                """
+                INSERT INTO line_items (item_name, price, quantity, trip_id)
+                VALUES (:name, :price, :quantity, :trip_id)
+                RETURNING id
+                """
+            ), {"name": item.name, "price": item.price, "quantity": item.quantity, "trip_id": trip_id}).scalar_one()
+            connection.execute(sqlalchemy.text(
+                """
+                INSERT INTO line_item_members (user_id, line_item_id)
+                SELECT user_id, :line_item_id FROM group_members
+                WHERE (user_id != ANY(:optout)
+                OR :optout is null)
+                AND group_id = :group_id
+                """
+            ), {"line_item_id": line_item_id, "optout": item.optedOut or None, "group_id": group_id})
+
+        transaction_id = connection.execute(sqlalchemy.text(
+            """
+            INSERT INTO transactions (description, trip_id)
+            VALUES (:desc, :trip)
+            RETURNING id
+            """
+        ), {"desc": "Paid for trip", "trip": trip_id}).scalar_one()
+
+        connection.execute(sqlalchemy.text(
+            """
+            INSERT INTO transaction_ledger
+            (transaction_id, to_id, from_id, change)
+              SELECT :transaction_id, line_item_members.user_id, :from_id, SUM(price * quantity) AS amount
+              FROM line_items
+              JOIN line_item_members ON line_items.id = line_item_members.line_item_id
+              GROUP BY line_item_members.user_id
+            """
+        ), {"transaction_id": transaction_id, "from_id": int(body.userId)})
+        return "OK"
+
+class Body(BaseModel):
+    userId: str
+@router.post("/calculate")
+def calculate(body: Body):
+  with db.engine.begin() as connection:
+    res = connection.execute(sqlalchemy.text(
+        """
+        SELECT line_item_members.user_id, :from_id, SUM(price * quantity) AS amount
+        FROM line_items
+        JOIN line_item_members ON line_items.id = line_item_members.line_item_id
+        GROUP BY line_item_members.user_id
+        """
+    ), {"from_id": int(body.userId)})
+    return [{"userId": row.user_id, "amount": row.amount} for row in res]
